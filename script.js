873071e94eacdb648952828d0ec3d55baab4fc63
raj210809 <145861977+raj210809@users.noreply.github.com>
2024-02-29 14:43:31 +0530

    feat : adds obstacles and its animation

--- script.js
+++ script.js
@@ -41,3 +41,29 @@ car.addEventListener("keydown", function (e) {
     }
   }
 });
+const lanes = [1, 2, 3];
+let selectedlane = lanes[Math.floor(Math.random() * lanes.length)];
+
+const obstacle = document.createElement("div");
+obstacle.className = "obstacle";
+
+obstacle.style.top = "0";
+obstacle.style.marginLeft = "1rem";
+document.getElementById("road").appendChild(obstacle);
+obstacle.classList.add("obstacle-animation");
+
+const obstacle2 = document.createElement("div");
+obstacle2.className = "obstacle";
+obstacle2.style.top = "0";
+obstacle2.style.marginLeft = "1rem";
+
+document.getElementById("horiz2").appendChild(obstacle2);
+obstacle2.classList.add("obstacle-animation");
+
+const obstacle3 = document.createElement("div");
+obstacle3.className = "obstacle";
+obstacle3.style.top = "0";
+obstacle3.style.marginLeft = "11rem";
+
+document.getElementById("road").appendChild(obstacle3);
+obstacle3.classList.add("obstacle-animation");
